1dd0e30923b318b437fa3db378ec505d0a0f5721
Piotr Persona <persona.piotr@gmail.com>
2019-04-13 23:03:08 +0200

    Introduce tests
    Closes: #9
    
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

---
+++ tests/test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -eu
+
+PROJECT_ROOT="$( git rev-parse --show-toplevel )"
+SCRIPT_PATH="${PROJECT_ROOT}/tests"
+DISTROS=( $( ls "${SCRIPT_PATH}/docker" ) )
+
+build_distro_image() {
+    local distro="${1}"
+    local distro_image="${2}"
+
+    local distro_docker="${SCRIPT_PATH}/docker/${distro}"
+
+    docker build \
+        -t "${distro_image}" \
+        -f "${distro_docker}/Dockerfile" \
+        "${distro_docker}"
+
+    echo "${distro_image}"
+}
+
+run_tests() {
+    local image="${1}"
+
+    container_name="$(
+        docker run -d -t -v "${PROJECT_ROOT}:/project" "${image}"
+    )"
+    docker exec -it "${container_name}" sh -c '/project/setup.sh'
+}
+
+main() {
+    for distro in "${DISTROS[@]}"; do
+        distro_image="${distro}-environment"
+        build_distro_image "${distro}" "${distro_image}"
+        run_tests "${distro_image}"
+    done
+}
+
+main
