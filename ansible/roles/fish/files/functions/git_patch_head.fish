ed14348c5dab14d07a66cf45ab6dc609c6c9ef06
Piotr Persona <persona.piotr@gmail.com>
2019-06-21 16:24:05 +0200

    fixup! Implement #16

--- ansible/roles/fish/files/functions/git_patch_head.fish
+++ ansible/roles/fish/files/functions/git_patch_head.fish
@@ -1,10 +1,5 @@
 function git_patch_head
-  set NON_FIXUP_HEAD_COMMIT "(
-    git log --oneline
-    | grep --invert-match 'fixup'
-    | head -1
-    | awk '{print $1}'
-  )"
+  set NON_FIXUP_HEAD_COMMIT ( git log --oneline | grep --invert-match 'fixup' | head -1 | awk '{print $1}' )
   git commit --fixup "$NON_FIXUP_HEAD_COMMIT"
   git rebase --interactive --autosquash "$NON_FIXUP_HEAD_COMMIT~1"
 end
