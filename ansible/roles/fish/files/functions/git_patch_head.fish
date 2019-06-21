ba60ce99121491fc25c12037a684bb185c58fd4a
Piotr Persona <persona.piotr@gmail.com>
2019-06-21 16:11:00 +0200

    Implement #16
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- ansible/roles/fish/files/functions/git_patch_head.fish
+++ ansible/roles/fish/files/functions/git_patch_head.fish
@@ -1,4 +1,10 @@
 function git_patch_head
-  git commit --fixup "( git rev-parse HEAD )"
-  git rebase -i --autosquash "( git rev-parse HEAD~2 )"
+  NON_FIXUP_HEAD_COMMIT="$(
+    git log --oneline
+    | grep --invert-match 'fixup'
+    | head -1
+    | awk '{print $1}'
+  )"
+  git commit --fixup "$NON_FIXUP_HEAD_COMMIT"
+  git rebase --interactive --autosquash "$NON_FIXUP_HEAD_COMMIT~1"
 end
