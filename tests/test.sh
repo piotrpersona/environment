984bf97f27aa9fcddab63ceab6bf51bec29c5c21
Piotr Persona <persona.piotr@gmail.com>
2019-07-20 19:44:41 +0200

    Pass test args to setup.sh
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- tests/test.sh
+++ tests/test.sh
@@ -22,25 +22,17 @@ build_distro_image() {
 
 run_tests() {
     local image="${1}"
+    local test_args="${@:2}"
+
+    echo "${test_args}"
 
     container_name="$(
         docker run -d -t -v "${PROJECT_ROOT}:/project:ro" "${image}"
     )"
 
-    docker exec -it "${container_name}" sh -c '/project/setup.sh'
-
-    # local temp_out_file="/tmp/${container_name}.log"
-    # docker exec -it "${container_name}" sh -c '/project/setup.sh' \
-    # 2>&1 "${temp_out_file}" | tee "${temp_out_file}"
-
-    # local changed="$( tail -10 "${temp_out_file}" | \
-    #     grep -Po '(?<=changed=)[0-9]+' )"
-
-    # echo "***************************** "
-    # [[ "${changed}" -ne "0" ]] \
-    #     && echo "NOT IDEMPOTENT" \
-    #     || echo "IDEMPOTENT"
-    # echo "***************************** "
+    docker exec -it "${container_name}" sh -c "/project/setup.sh ${test_args}" \
+        && echo "success" \
+        || echo "failure"
 
     docker rm -f "${container_name}"
 }
@@ -49,8 +41,8 @@ main() {
     for distro in "${DISTROS[@]}"; do
         distro_image="${distro}-environment"
         build_distro_image "${distro}" "${distro_image}"
-        run_tests "${distro_image}"
+        run_tests "${distro_image}" "${@}"
     done
 }
 
-main
+main "${@}"
