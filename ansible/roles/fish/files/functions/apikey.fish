bd51e95c827a46bdcdcb5947513bf9457e463146
Piotr Persona <persona.piotr@gmail.com>
2021-12-08 13:37:56 +0100

    fix: Apikey func
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- ansible/roles/fish/files/functions/apikey.fish
+++ ansible/roles/fish/files/functions/apikey.fish
@@ -1,3 +1,3 @@
 function apikey
-    python3 -c 'import secrets; import sys; print(secrets.token_urlsafe(int(sys.argv[1])))' "${1}"
+    python3 -c 'import secrets; import sys; print(secrets.token_urlsafe(int(sys.argv[1])))' $argv[1]
 end
