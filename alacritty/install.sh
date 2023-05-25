51e46a88b20f24a38dfd81c0298d01d3d451c220
Piotr Persona <piotrpersona@silversurfer.local>
2023-05-25 23:57:39 +0200

    fix: alacritty
    Signed-off-by: Piotr Persona <piotrpersona@silversurfer.local>

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ALA_HOME="${HOME}/.config/alacritty"}
+ALA_HOME="${HOME}/.config/alacritty"
 mkdir -p "${ALA_HOME}"
 cp -f "$( dirname ${0})/alacritty.yml" "${ALA_HOME}/alacritty.yml"
