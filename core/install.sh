e434e1a996b4e0acf6d4c9da4e66e16d971ab120
Piotr Persona <persona.piotr@gmail.com>
2023-05-26 08:43:21 +0200

    feat: Add key repeat
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- core/install.sh
+++ core/install.sh
@@ -2,3 +2,6 @@
 brew  -v || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 mkdir -p ~/work ~/developer ~/.ssh
+
+defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
