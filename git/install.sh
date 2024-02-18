549d88d4c9414f5f235901a5528c87e2c336f1cb
Piotr Persona <persona.piotr@gmail.com>
2024-02-18 15:39:22 +0100

    install gitignore
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 GITCONFIG="${HOME}/.gitconfig"
 GITIGNORE="${HOME}/.gitignore"
-[[ -e "${GITCONFIG}" ]] || cp -f "$( dirname ${0})/.gitconfig" "${GITCONFIG}"
-[[ -e "${GITIGNORE}" ]] || cp -f "$( dirname ${0})/.gitignore" "${GITIGNORE}"
+cp -f "$( dirname ${0})/.gitconfig" "${GITCONFIG}"
+cp -f "$( dirname ${0})/.gitignore" "${GITIGNORE}"
 
