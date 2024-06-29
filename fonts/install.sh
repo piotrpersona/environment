be281d6387e52b72cd926a44752d8ad39f925a6c
Piotr Persona <persona.piotr@gmail.com>
2024-06-29 23:24:27 +0200

    feat: Install fonts if needed
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-brew tap homebrew/cask-fonts
-brew search '/font-.*-nerd-font/' | awk '{ print $1 }' | xargs -I{} brew install --cask {} || true
+
+ls ~/Library/Fonts | grep -i nerd > /dev/null
+if [ $? -ne 0 ]; then
+    brew tap homebrew/cask-fonts \
+    && brew search '/font-.*-nerd-font/' | awk '{ print $1 }' | xargs -I{} brew install --cask {} || true
+else
+    echo "Nerd fonts already installed"
+fi
