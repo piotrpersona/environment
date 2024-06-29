c2597277ac3b1f70abe02ec651b337f55e41db14
Piotr Persona <persona.piotr@gmail.com>
2024-06-29 18:05:02 +0200

    fix: gitconfig
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-GITCONFIG="${HOME}/.gitconfig"
 GITIGNORE="${HOME}/.gitignore"
-cp -f "$( dirname ${0})/.gitconfig" "${GITCONFIG}"
 cp -f "$( dirname ${0})/.gitignore" "${GITIGNORE}"
+
+git config --global core.editor nvim
+git config --global core.whitespace "fix,-indent-with-non-tab,trailing-space,cr-at-eol"
+git config --global core.excludesfile ~/.gitignore
 
