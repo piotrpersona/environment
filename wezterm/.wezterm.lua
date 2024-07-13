31a919673a0342045eac5d8e4137887cc28888ba
Piotr Persona <persona.piotr@gmail.com>
2024-07-13 11:18:29 +0200

    feat: Jump words with alt
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -17,9 +17,10 @@ config.send_composed_key_when_left_alt_is_pressed = true
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 config.keys = {
- -- Navigate words with Alt key
-	{ key = "LeftArrow", mods = "OPT", action = wezterm.action{ActivateCopyModeLeftWord=true} },
-	{ key = "RightArrow", mods = "OPT", action = wezterm.action{ActivateCopyModeRightWord=true} },
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
 
 	-- Delete words with Alt + Backspace
 	{ key = "Backspace", mods = "OPT", action = wezterm.action{SendString="\x17"} }, -- \x17 is Ctrl+W (delete word)
